bb04ce4f6b772d86af73f340db53a43b5684a848
steeltx <realsteel.gnu@gmail.com>
2021-10-17 23:44:17 -0500

    Iniciando módulo Vuex de Journal

---
+++ src/modules/daybook/store/journal/mutations.js
@@ -0,0 +1,17 @@
+
+
+// export const myAction = ( state ) => {
+
+// }
+
+export const setEntries = ( /*state*/ ) => {
+
+}
+
+export const updateEntry = ( /*state*/ ) => {
+    
+}
+
+export const addEntry = ( /*state*/ ) => {
+    
+}
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,7 @@
+
+export default () => ({
+
+    isLoading: true,
+    entries: [1,2,3,4,5,6]
+
+})
